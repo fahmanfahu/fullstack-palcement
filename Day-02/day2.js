cb8394519472bb452ac0c441be0dbc01e56c894f
Fahman <fahmanfahu90@gmail.com>
2026-09-19 01:05:00 +0530

    Complete Day-2 javaScript conditions

---
+++ Day-02/day2.js
@@ -0,0 +1,85 @@
+/*
+let age = 21;
+if(age >= 18)
+{
+    console.log("you are eligible for voting");
+}
+let Age = 17;
+if(Age >= 18)
+{
+    console.log("you are not eligible");
+}
+else
+{
+    console.log("you are eligible");
+}*/
+
+/*
+let mark = 75;
+if (mark >= 90)
+{
+    console.log("A+garade");
+}
+else if (mark >= 80)
+{
+    console.log("Agreade");
+}
+else if (mark >= 70)
+{
+    console.log("Bgreade");
+}
+else if(mark >=60)
+{
+    console.log("Cgreade");
+}
+else{
+    console.log("fail");
+}
+    */
+/*
+let age = 21;
+console.log(age > 18);
+console.log(age < 18);
+console.log(age === 21);
+console.log(age !== 21);
+*/
+/*
+let password = "1234";
+if (password === "1234")
+{
+    console.log("login sussessful");
+}
+else
+{
+    console.log("wrong password");
+}
+    */
+/*
+let age = 21;
+let hasID = true;
+if (age >= 18 && hasID === true)
+{
+    console.log("entry granted");
+}
+else
+{
+    console.log("entry dinied");
+}
+    */
+/*
+let role = "admin";
+if (role ==="admin" || role === "teacher")
+{
+    console.log("access granted");
+}
+    */
+
+let age = 21;
+if(age >= 18)
+{
+    console.log(" Adult");
+}
+else 
+{
+    console.log("minor");
+}
---
+++ Day-02/day2task.js
@@ -0,0 +1,49 @@
+/*
+let mark = 85;
+if(mark >= 90)
+{
+    console.log("A+ grade");
+}
+else if (mark >= 80)
+{
+    console.log("A grade");
+}
+else if (mark >= 70)
+{
+    console.log("B grade");
+}
+else if (mark >= 60)
+{
+    console.log("C grade");
+}
+else
+{
+    console.log("Fail");
+}
+    */
+/*
+let number = 10;
+if (number === 0)
+{
+    console.log("zero");
+}
+else if (number > 0)
+{
+    console.log("+ve");
+}
+else
+{
+    console.log("-ve");
+}
+    */
+
+let udername = "fahman";
+let password = "1234";
+if (udername === "fahman" && password === "1234")
+{
+    console.log("login successful");
+}   
+else
+{
+    console.log("login failed");
+}
