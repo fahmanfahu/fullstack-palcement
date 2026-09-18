8984073c4d5fe9de92013114d64860544cdab201
Fahman <fahmanfahu90@gmail.com>
2026-09-18 21:33:52 +0530

    Day 1 - javaScript basics

---
+++ Day-01/intex.js
@@ -0,0 +1,37 @@
+console.log("hello, i am fahman!");
+console.log("iam startiing my full stack journy");
+// variable
+
+let name = "fahman";
+let age = 21;
+console.log(name);
+console.log(age);
+
+//number
+let a = 10;
+let b = 20;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+//string
+let Name = "fahman"
+let course = "computer science"
+console.log(Name);
+console.log(course);
+
+//boolean
+let isStudent = true;
+let isTeacher = false;
+console.log(isStudent);
+console.log(isTeacher);
+
+//simple calculation
+let mark1 = 80;
+let mark2 = 90;
+let mark3 = 70;
+let total = mark1 + mark2 + mark3;
+let average = total/3;
+console.log(" Total marks : " +total);
+console.log(" Avg marks : " +average);
---
+++ Day-01/practice.js
@@ -0,0 +1,20 @@
+let name = " fahman";
+let age = 21;
+let collage = "sahrdaya collage";
+let branch ="computer science";
+console.log(name);
+console.log(age);
+console.log(collage);
+console.log(branch);
+
+let a = 25;
+let b = 10;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+let totalmarks = 450;
+let maxmarks = 500;
+let percentage = (totalmarks/maxmarks)*100;
+console.log("pers : ",percentage);
